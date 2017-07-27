72d1d6056e3577fd81b537544c8725bd37516b2d
Matthew Bell <m.bell@cs.ucl.ac.uk>
2017-07-27 20:51:09 +0100

    Make app navigation lengthy

--- client/app/bundles/HackerView/components/Navigation/index.jsx
+++ client/app/bundles/HackerView/components/Navigation/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navigation = () => (
-  <nav>
+  <nav style={{ display: "flex", flexDirection: "row" }}>
     <p><Link to="/">Home</Link></p>
     <p><Link to="/test">Test</Link></p>
     <p><a rel="nofollow" data-method="delete" href="/auth/sign_out">Sign Out</a></p>
