13387bc04ec2732fb16ab72852de62f2c4af6645
Matthew Bell <m.bell@cs.ucl.ac.uk>
2017-07-27 20:27:03 +0100

    Add test page to HackerView

---
+++ client/app/bundles/HackerView/components/TestPage/index.jsx
@@ -0,0 +1,11 @@
+/* eslint react/prop-types: 0 */
+import React from "react";
+
+const TestPage = () => (
+  <div>
+    <h1>Test Page</h1>
+    <p>This is a test page</p>
+  </div>
+);
+
+export default TestPage;
