4cce4742db1e5914f6ff1ddd31a8267dc5d73033
Matthew Bell <m.bell@cs.ucl.ac.uk>
2017-08-04 12:34:12 +0100

    Update registration controller to allow first/last names

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name { Faker::StarWars.character }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
     password { Faker::Internet.password }
   end
