680bc67f512781f1789fba8356de4be263aa287b
Matthew Bell <m.bell@cs.ucl.ac.uk>
2017-11-03 19:30:08 +0000

    Fix conditional statement in build script

--- build-docker-prod.sh
+++ build-docker-prod.sh
@@ -3,7 +3,7 @@
 cat .env.common > .env
 cat .env.prod >> .env
 
-if [ $2 -eq "--deploy" ]; then
+if [ "$2" == "--deploy" ]; then
   docker-compose -f common.yml -f docker-compose-prod.yml up -d --build
 else
   docker-compose -f common.yml -f docker-compose-prod.yml build
