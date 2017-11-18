66cf33ba4e742ff1323e2733bc954b29e5b63333
Matthew Bell <matt@mbell.me>
2017-11-18 13:18:19 +0000

    Make notifications animated

--- client/app/libs/components/Notifications/index.jsx
+++ client/app/libs/components/Notifications/index.jsx
@@ -1,20 +1,26 @@
+/* eslint react/no-array-index-key: 0 */
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { generate } from "shortid";
+import { FadeInOut, Stagger } from "react-animation-components";
 import { seenNotification } from "libs/actions/notificationsActions";
 import Notification from "./Notification";
 
 const Notifications = ({ notifications, onClick }) => (
   <div className="notification-container">
-    {notifications.map(notification => (
-      <Notification
-        {...notification}
-        type={notification.notification_type}
-        key={generate()}
-        onCloseClick={() => onClick(notification.id)}
-      />
-    ))}
+    <Stagger delay={50}>
+      {notifications.map((notification, i) => (
+        <FadeInOut key={i}>
+          <Notification
+            {...notification}
+            type={notification.notification_type}
+            key={generate()}
+            onCloseClick={() => onClick(notification.id)}
+          />
+        </FadeInOut>
+      ))}
+    </Stagger>
   </div>
 );
 
